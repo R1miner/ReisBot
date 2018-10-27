abf711921373c90d579b5a88eb9a6c8ec8972b93
R1miner <r1miner@outlook.com>
2018-10-27 23:46:19 +0200

    test?

--- index.js
+++ index.js
@@ -4,22 +4,6 @@ const fs = require("fs");
 const bot = new Discord.Client();
 bot.commands = new Discord.Collection();
 
-fs.readdir("./commands",(err,files)=>{
-    if(err)console.log(err);
-
-    let jsfile = files.filter(f => f.split(".").pop()==="js")
-    if(jsfile.lenght <= 0){
-        console.log("Konnte Datei nicht finden!")
-        return;
-    }
-
-    jsfile.forEach((f,i)=>{
-       let props = require(`.commands/${f}`);
-       console.log(`${f} loaded`);
-       bot.commands.set(props.help.name,props);
-    });
-});
-
 bot.on("ready",async()=>{
     console.log(`${bot.user.username} is online!`);
     bot.user.setActivity("Abonniert ReisMiner auf YT!")
