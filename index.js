7d58cb10cdc952e5168517955b6907cbf5de3318
R1miner <r1miner@outlook.com>
2018-10-27 23:44:48 +0200

    test?

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const botconfig = require("./botconfig");
 const Discord = require("discord.js");
 const fs = require("fs");
-const bot = new Discord.Client({disableEveryone: true});
+const bot = new Discord.Client();
 bot.commands = new Discord.Collection();
 
 fs.readdir("./commands",(err,files)=>{
