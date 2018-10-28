4e953d4194114732fb5b9f14d1da584e4b7242a5
R1miner <r1miner@outlook.com>
2018-10-28 19:22:01 +0100

    test?

--- index.js
+++ index.js
@@ -2,14 +2,6 @@ const botconfig = require("./botconfig");
 const Discord = require("discord.js");
 
 var bot = new Discord.Client();
-function normalText(){
-    if(message.content=="oke"||message.content=="Oke"){
-        message.channel.send("oke");
-    }
-    if(message.content=="hmm"||message.content=="hmm"){
-        message.channel.send(":D");
-    }
-}
 
 bot.on("ready", async () => {
     console.log(`${bot.user.username} is online!`);
@@ -18,7 +10,13 @@ bot.on("ready", async () => {
 
 bot.on("message",function(message){
     if(message.author.equals(bot.user))return;
-    normalText();
+
+    if(message.content=="oke"||message.content=="Oke"){
+        message.channel.send("oke");
+    }
+    if(message.content=="hmm"||message.content=="hmm"){
+        message.channel.send(":D");
+    }
 
     var args = message.content.substring(botconfig.prefix.length).split(" ");
 
