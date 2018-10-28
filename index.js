39d932eb5df0842599e66f5d029c37fc4b9ea09b
R1miner <r1miner@outlook.com>
2018-10-28 19:09:07 +0100

    test?

--- index.js
+++ index.js
@@ -3,18 +3,19 @@ const Discord = require("discord.js");
 
 var bot = new Discord.Client();
 
-bot.on("ready",function () {
-    bot.setActivity("Abonniert ReisMiner auf YT!")
+bot.on("ready", async () => {
+    console.log(`${bot.user.username} is online!`);
+    bot.user.setActivity("Abonniert ReisMiner auf YT!")
 });
 
 bot.on("message",function(message){
     if(message.author.equals(bot.user))return;
 
     if(message.content=="oke"||message.content=="Oke"){
-        message.channel.sendMessage("oke");
+        message.channel.send("oke");
     }
     if(message.content=="hmm"||message.content=="hmm"){
-        message.channel.sendMessage("oke");
+        message.channel.send(":D");
     }
 });
 bot.login(process.env.BOT_TOKEN);
