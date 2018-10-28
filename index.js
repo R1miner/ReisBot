d0d4700abd2aa71a8099c0eb79976058f5db6f69
R1miner <r1miner@outlook.com>
2018-10-28 20:39:43 +0100

    Rolle

--- index.js
+++ index.js
@@ -5,11 +5,8 @@ var bot = new Discord.Client();
 
 
 bot.on(`guildMemberAdd`, member =>{
-    member.guild.channels.find("name", "main-chat").send(member.toString()+" Wilkommen auf dem Discordserver und viel spass in der Community");
+    member.guild.channels.find("name", "main-chat").send(member.toString()+" Wilkommen auf dem Discordserver und viel spass in der Community")
     let role = message.guild.roles.find(r => r.name === "Zuschauer");
-    let person = message.mentions.members.first();
-    person.addRole(role).catch(console.error)
-
     member.addRole(role);
 });
 
