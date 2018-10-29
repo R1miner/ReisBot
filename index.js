e038c4862a8e326e262e301c49d64bed43532a03
R1miner <r1miner@outlook.com>
2018-10-29 16:08:26 +0100

    Rolle

--- index.js
+++ index.js
@@ -1,41 +1,69 @@
 const botconfig = require("./botconfig");
 const Discord = require("discord.js");
+const autoroleName = 'Zuschauer';
+const interval =10;
 
 var bot = new Discord.Client();
 
 
-bot.on("serverNewMember", function (server, user) {
-    mybot.addMemberToRole(user, server.roles.get("name", "RoleName"), function (err) {
-        if (err) console.log(err)
-        member.guild.channels.find("name", "main-chat").send(member.toString() + " Wilkommen auf dem Discordserver und viel spass in der Community");
-    });
+bot.on('guildMemberAdd', member => {
+    member.guild.channels.find("name", "main-chat").send(member.toString()+" Wilkommen auf dem Discordserver und viel spass in der Community");
+});
 
-    bot.on("ready", async () => {
-        console.log(`${bot.user.username} is online!`);
-        bot.user.setActivity("Abonniert ReisMiner auf YT!");
-    });
+bot.on("ready", async () => {
+    console.log(`${bot.user.username} is online!`);
+    bot.user.setActivity("Abonniert ReisMiner auf YT!");
 
-    bot.on("message", function (message) {
-        if (message.author.equals(bot.user)) return;
+    var AutoAddRole = Roles.find('name',autoroleName);
 
-        if (message.content == "oke" || message.content == "Oke") {
-            message.channel.send("oke");
-            return;
+    setInterval(function(){
+        var i;
+        for( i in client.users.array() ){
+            var User = client.users.array()[i];
+            var has_role = false;
+            // Search users without roles
+            if( User.bot == false ) {
+                var r;
+                for(r in Roles.array() ) {
+                    var Role = Roles.array()[r];
+                    if(Role.name != '@everyone') {
+                        var Rolecheck = Role.members.find('id', User.id);
+                        if(Rolecheck !== null) {
+                            has_role = true;
+                            break;
+                        }
+                    }
+                }
+                if(!has_role && User.presence.status == 'online') {
+                    // Get member
+                    var Member = client.guilds.array()[0].members.find('id', User.id);
+                    Member.addRole(AutoAddRole.id);
+                }
+            }
         }
-        if (message.content == "hmm" || message.content == "Hmm") {
-            message.channel.send(":D");
-            return;
-        }
+    }, interval);
+});
 
-        var args = message.content.substring(botconfig.prefix.length).split(" ");
+bot.on("message", function (message) {
+    if (message.author.equals(bot.user)) return;
 
-        switch (args[0].toLowerCase()) {
-            case"ping":
-                message.channel.send("Zu Hoch :grinning:");
-                break;
-        }
+    if (message.content == "oke" || message.content == "Oke") {
+        message.channel.send("oke");
+        return;
+    }
+    if (message.content == "hmm" || message.content == "Hmm") {
+        message.channel.send(":D");
+        return;
+    }
 
-    });
+    var args = message.content.substring(botconfig.prefix.length).split(" ");
 
-    bot.login(process.env.BOT_TOKEN);
-}
+    switch (args[0].toLowerCase()) {
+        case"ping":
+            message.channel.send("Zu Hoch :grinning:");
+            break;
+    }
+
+});
+
+bot.login(process.env.BOT_TOKEN);
