f28c149dc6036637b7c7349ad02f4c5c9435bef7
R1miner <r1miner@outlook.com>
2018-10-29 20:01:19 +0100

    Rolle

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ bot.on("ready", async () => {
 function play(connection, message) {
     var server = servers[message.guild.id];
 
-    server.dispatcher = connection.playStream(YTDL(server.queue[0], {filter: "audioonly", quality:"lowestaudio"}));
+    server.dispatcher = connection.playStream(YTDL(server.queue[0], {filter: "audioonly"}));
     server.queue.shift();
 
     server.dispatcher.on("end", function () {
