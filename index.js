ac0978f083cfe21d94c844b0ffa8e3876679dbe3
R1miner <r1miner@outlook.com>
2018-10-29 16:29:50 +0100

    Rolle

--- index.js
+++ index.js
@@ -1,47 +1,18 @@
 const botconfig = require("./botconfig");
 const Discord = require("discord.js");
-const autoroleName = 'Zuschauer';
-const interval =10;
 
 var bot = new Discord.Client();
 
 
 bot.on('guildMemberAdd', member => {
+    var role = member.guild.roles.find("name", "Zuschauer");
+    member.addRole(role);
     member.guild.channels.find("name", "main-chat").send(member.toString()+" Wilkommen auf dem Discordserver und viel spass in der Community");
 });
 
 bot.on("ready", async () => {
     console.log(`${bot.user.username} is online!`);
     bot.user.setActivity("Abonniert ReisMiner auf YT!");
-
-    var AutoAddRole = Roles.find('name',autoroleName);
-
-    setInterval(function(){
-        var i;
-        for( i in client.users.array() ){
-            var User = client.users.array()[i];
-            var has_role = false;
-            // Search users without roles
-            if( User.bot == false ) {
-                var r;
-                for(r in Roles.array() ) {
-                    var Role = Roles.array()[r];
-                    if(Role.name != '@everyone') {
-                        var Rolecheck = Role.members.find('id', User.id);
-                        if(Rolecheck !== null) {
-                            has_role = true;
-                            break;
-                        }
-                    }
-                }
-                if(!has_role && User.presence.status == 'online') {
-                    // Get member
-                    var Member = client.guilds.array()[0].members.find('id', User.id);
-                    Member.addRole(AutoAddRole.id);
-                }
-            }
-        }
-    }, interval);
 });
 
 bot.on("message", function (message) {
