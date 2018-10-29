fbe7d1fbe15ddb981e52e9475ee7597224575771
R1miner <r1miner@outlook.com>
2018-10-29 16:00:31 +0100

    Rolle

--- index.js
+++ index.js
@@ -4,35 +4,38 @@ const Discord = require("discord.js");
 var bot = new Discord.Client();
 
 
-bot.on('guildMemberAdd', (guildMember) => {
-    guildMember.addRole(guildMember.guild.roles.find(role => role.name === "Zuschauer"));
-});
+bot.on("serverNewMember", function (server, user) {
+    mybot.addMemberToRole(user, server.roles.get("name", "RoleName"), function (err) {
+        if (err) console.log(err)
+        member.guild.channels.find("name", "main-chat").send(member.toString() + " Wilkommen auf dem Discordserver und viel spass in der Community");
+    });
 
-bot.on("ready", async () => {
-    console.log(`${bot.user.username} is online!`);
-    bot.user.setActivity("Abonniert ReisMiner auf YT!");
-});
+    bot.on("ready", async () => {
+        console.log(`${bot.user.username} is online!`);
+        bot.user.setActivity("Abonniert ReisMiner auf YT!");
+    });
 
-bot.on("message", function (message) {
-    if (message.author.equals(bot.user)) return;
+    bot.on("message", function (message) {
+        if (message.author.equals(bot.user)) return;
 
-    if (message.content == "oke" || message.content == "Oke") {
-        message.channel.send("oke");
-        return;
-    }
-    if (message.content == "hmm" || message.content == "Hmm") {
-        message.channel.send(":D");
-        return;
-    }
+        if (message.content == "oke" || message.content == "Oke") {
+            message.channel.send("oke");
+            return;
+        }
+        if (message.content == "hmm" || message.content == "Hmm") {
+            message.channel.send(":D");
+            return;
+        }
 
-    var args = message.content.substring(botconfig.prefix.length).split(" ");
+        var args = message.content.substring(botconfig.prefix.length).split(" ");
 
-    switch (args[0].toLowerCase()) {
-        case"ping":
-            message.channel.send("Zu Hoch :grinning:");
-            break;
-    }
+        switch (args[0].toLowerCase()) {
+            case"ping":
+                message.channel.send("Zu Hoch :grinning:");
+                break;
+        }
 
-});
+    });
 
-bot.login(process.env.BOT_TOKEN);
+    bot.login(process.env.BOT_TOKEN);
+}
